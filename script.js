f33f36c95edebd306bf914f69c8d86cdbf811906
Muhammad Mustafa <mustafalada36@gmail.com>
2023-02-06 01:02:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
  var message="In Global";
-  var message="In Global 2";
+
  var a = function  (){
  	var message="Inside a";
  	console.log("message a:" +message)
- }
- a();
 
- var b= function (){
+ 	 var b= function (){
  	console.log("message inside b:" +message)
  }
 
  b();
+ }
+ a();
+
+ //  var b= function (){
+ // 	console.log("message inside b:" +message)
+ // }
+
+ // b();
+
