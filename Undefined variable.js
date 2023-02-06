5dc32258aeffcd519d4047b655f3bb168fbe703b
Muhammad Mustafa <mustafalada36@gmail.com>
2023-02-06 19:56:31 +0500

    Update Undefined variable.js

--- Undefined variable.js
+++ Undefined variable.js
@@ -2,10 +2,10 @@ var c;
 c= 21;
 console.log(c);
 
-if(c==undefined){
-	console.log("Value is undefined");
-	console.log("\n");
-}
+// if(c==undefined){
+// 	console.log("Value is undefined");
+// 	console.log("\n");
+// }
 
 if (c==undefined) {
 	console.log("Value is undefined");
