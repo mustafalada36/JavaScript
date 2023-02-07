0d16e288969874ca19cbe0b1d4399f320eaa8be3
Muhammad Mustafa <mustafalada36@gmail.com>
2023-02-07 20:54:13 +0500

    Update objectcreation.js

--- objectcreation.js
+++ objectcreation.js
@@ -3,6 +3,9 @@ var student= new Object();
 student.name="M Ali";
 student["No of Students"]=10;
 
-student.bstudent.firstname= "Pasha";
 console.log(student);
 console.log(student.name);
+
+
+student.bstudent = new Object();
+student.bstudent.firstname= "Pasha";
