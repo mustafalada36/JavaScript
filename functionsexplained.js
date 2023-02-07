fad43ca6a1a95f63aed63573c2ab386c7f8fc0ca
Muhammad Mustafa <mustafalada36@gmail.com>
2023-02-08 03:39:41 +0500

    Update functionsexplained.js

--- functionsexplained.js
+++ functionsexplained.js
@@ -5,3 +5,11 @@
 	console.log(multiply(3,4));
 	 multiply.version = 3.0;
 	console.log(multiply.version);
+
+	//Function Factory
+
+	function makemultiplier (multiplier){
+		var myFunc = function (c){
+			return multiplier*c;
+		}
+	}
