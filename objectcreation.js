cc9cc31f0ca05c6c9422a72d4f746535abd82f97
Muhammad Mustafa <mustafalada36@gmail.com>
2023-02-07 21:07:04 +0500

    Update objectcreation.js

--- objectcreation.js
+++ objectcreation.js
@@ -12,6 +12,13 @@ student.bstudent.firstname= "Pasha";
 console.log(student.bstudent);
 // OR by Second Way
 var BStudentFirstName = "Nauman";
-student[BStudentFirstName] = 10;
-console.log("B Students are:"+student[BStudentFirstName]);
+var BStudent;
+student[BStudent] = 10;
+console.log("B Students are:"+student[BStudent]);
 
+//Third way
+
+var BStudentLastName = "Ali";
+var WStudent;
+student[WStudent] = 2;
+console.log("W Students are:" +student[WStudent]);
