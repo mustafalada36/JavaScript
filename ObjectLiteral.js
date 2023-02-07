0438aebfecce7ff537625fbdd86ff0661229be61
Muhammad Mustafa <mustafalada36@gmail.com>
2023-02-07 21:20:43 +0500

    Create ObjectLiteral.js

---
+++ ObjectLiteral.js
@@ -0,0 +1,13 @@
+
+//Better way for Object Creation
+
+	var student={
+		Name:"M ALI",
+		bstudent:{
+			firstname:"Nauman",
+			noofbstudents:10
+		},
+		noofwstudents:2
+	};
+	console.log(student);
+	console.log("B Students are:"+student.bstudent.noofbstudents);
