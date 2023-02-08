261ac3365cd2b9da0cc5d8ecad65e04132250227
Muhammad Mustafa <mustafalada36@gmail.com>
2023-02-08 21:40:23 +0500

    Update test.js

--- test.js
+++ test.js
@@ -7,11 +7,19 @@
 // let a=10;
 // console.log(a++);
 
-var sum=0;
-let a=10;
-for (a; a < 20; a++) {
-  sum =sum +a;
-    // console.log(a);
+// var sum=0;
+// let a=10;
+// for (a; a < 20; a++) {
+//   sum =sum +a;
+//     // console.log(a);
  
-}
-console.log("\nThe Sum is:" +sum);
+// }
+// console.log("\nThe Sum is:" +sum);
+
+var x = 5;
+var y = x;
+x = 10;
+console.log("Before " +y);
+
+var y=x;
+console.log("After" +y)
