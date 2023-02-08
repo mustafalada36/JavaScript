ff976d085bac039a7027f96dafb37f99897af43e
Muhammad Mustafa <mustafalada36@gmail.com>
2023-02-08 21:26:11 +0500

    Update passbyreference.js

--- passbyreference.js
+++ passbyreference.js
@@ -1,1 +1,26 @@
 //Objects can be passed by References
+
+var a ={
+	name:10;
+};
+var b =a;
+console.log(a);
+console.log(b);
+
+var b.name = 5;
+console.log("After:")
+console.log(a);
+console.log(b);
+
+
+// console.log("\n INTO FUNCTIONS");
+// //in Function Pass by value
+// function multiply(multi) {
+// 	console.log(multi);
+// 	multi=8;
+// 	console.log(multi);
+
+// }
+// 	var value = 2;
+// 	multiply(value);
+// 	console.log(value);
