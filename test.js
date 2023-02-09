5767a01b91c00415dffba036c0a56ad8b06c9bc0
Muhammad Mustafa <mustafalada36@gmail.com>
2023-02-09 06:43:09 +0500

    Update test.js

--- test.js
+++ test.js
@@ -16,10 +16,21 @@
 // }
 // console.log("\nThe Sum is:" +sum);
 
-var x = 5;
-var y = x;
-x = 10;
-console.log("Before " +y);
+// var x = 5;
+// var y = x;
+// x = 10;
+// console.log("Before " +y);
 
-var y=x;
-console.log("After" +y)
+// var y=x;
+// console.log("After" +y)
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.bark = function () {
+  console.log(this.name + " likes barking! Bark!");
+}
+
+var max = new Dog("Max", "Buddy");
+max.bark();
