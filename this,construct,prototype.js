6d849f4b826f7aebcfbcb583e1a96ddc5ea43d44
Muhammad Mustafa <mustafalada36@gmail.com>
2023-02-09 06:34:17 +0500

    Update this,construct,prototype.js

--- this,construct,prototype.js
+++ this,construct,prototype.js
@@ -2,19 +2,19 @@
 	function Circle(radius) {
 		
 		this.radius = radius;
-		this.getArea = 
-		function(){
-			return Math.PI *Math.pow(this.radius,2);
-		};
+		// this.getArea = 
+		// function(){
+		// 	return Math.PI *Math.pow(this.radius,2);
+		// };
 	};
-
-		Circle.prototype.getArea2 =
+		//Prototype
+		Circle.prototype.getArea =
 		 function() 
 		{
 			return Math.PI *Math.pow(this.radius,4);
 		};
 
 		var myCircle = new Circle(10);
-		console.log(myCircle.getArea());
+		console.log(myCircle);
 
-		console.log(myCircle.prototype.getArea2);
+		
