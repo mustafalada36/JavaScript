bf2d14762c1111f869a91af73d8a5c7f2c13c670
Muhammad Mustafa <mustafalada36@gmail.com>
2023-02-11 01:23:30 +0500

    Update app.js

--- fakenamespace/app.js
+++ fakenamespace/app.js
@@ -1,3 +1,18 @@
 		
 		Markgreeter.sayHello();
 		Woodgreeter.sayHi();
+
+	
+
+
+
+
+	
+
+
+	//Immediately Invoke Functions Expressions
+	//IIFE
+
+	// (function (name) {
+	// 	console.log("Name is :" +name)
+	// })("Coursera");
