31f93f998169446ff4c317a1100f24869428e50e
Muhammad Mustafa <mustafalada36@gmail.com>
2023-02-15 04:11:06 +0500

    Update script.js

--- Build Website using JS/script.js
+++ Build Website using JS/script.js
@@ -2,4 +2,4 @@
 	console.log(window.document.getElementById("title"));
 	console.log(document instanceof HTMLDocument);
 	console.log(window.document.getElementById("name");
-	console.log();
+	console.log(This is called as );
