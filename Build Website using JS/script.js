74e281a2ef54edc785c2a5ed3411aad5591b8e67
Muhammad Mustafa <mustafalada36@gmail.com>
2023-02-17 23:00:58 +0500

    Update script.js

--- Build Website using JS/script.js
+++ Build Website using JS/script.js
@@ -1,5 +1,5 @@
 
-	console.log(window.document.getElementById("title"));
-	console.log(document instanceof HTMLDocument);
-	console.log(window.document.getElementById("name");
-	console.log(This is called as DOM Manipulation in single page web application);
+	// console.log(window.document.getElementById("title"));
+	// console.log(document instanceof HTMLDocument);
+	// console.log(window.document.getElementById("name");
+	// console.log(This is called as DOM Manipulation in single page web application);
