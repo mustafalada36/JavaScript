facada13664e075d3fd52b3592042a8e069c64a6
Muhammad Mustafa <mustafalada36@gmail.com>
2023-02-18 00:58:31 +0500

    Update script.js

--- Build Website using JS/script.js
+++ Build Website using JS/script.js
@@ -8,7 +8,7 @@
 		var name = document.getElementById("name").value;
 
 		// var message = "<h2>Hello  " +name +" !</h2>";
-
+		//OR Second WAY
 		var message = "<h2>" +name +"</h2>";
 
 			 document.getElementById("content").innerHTML = message;
