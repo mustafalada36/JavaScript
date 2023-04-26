3a9ea12164d649b48cf5e6a0113195a3a9307769
Muhammad Mustafa <mustafalada36@gmail.com>
2023-04-26 14:09:15 +0500

    update

---
+++ Classwork/javascriptbasics.js
@@ -0,0 +1,17 @@
+
+var a = 10;
+var b = "Java";
+console.log("Hello World" );
+console.log(" ");
+if (b=="Java") {
+
+	console.log("B is Printed");
+
+} else {
+	console.log("10 ");
+}
+console.log(" ");
+for (var i = 0; i <=5 ; i++) {
+	
+	console.log("The value of I is :" +i);
+}
