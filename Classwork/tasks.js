089a329a69ffcef9a5f03ba65d24c5a692efe4fe
Muhammad Mustafa <mustafalada36@gmail.com>
2023-04-28 19:23:39 +0500

    Update tasks.js

--- Classwork/tasks.js
+++ Classwork/tasks.js
@@ -8,6 +8,9 @@
 	// const c = prompt("Multiplication is:",a*b) ;
 	// const d = prompt("Division is:",a/b);
 
+
+
+
 	// Q2
  	// function sumandtriple(num1,num2){
  	// 	var sum = num1+num2;
@@ -15,11 +18,14 @@
  	// 		sum *=3;
  	// 	}
  	// 	document.write("Sum = " + sum);
-
  	//  };
  	//  sumandtriple(4,4)();
  	
 
-	
+
+
+	// Q3
+	const par = document.getElementById("para");
+	par.style.backgroundColor = "Yellow";
   
  	 
