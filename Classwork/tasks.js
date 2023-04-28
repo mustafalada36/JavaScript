e5b573dec2beec2251a2c3138ae54a2d3febab5c
Muhammad Mustafa <mustafalada36@gmail.com>
2023-04-28 18:49:43 +0500

    update

--- Classwork/tasks.js
+++ Classwork/tasks.js
@@ -1,11 +1,21 @@
 	
-	let a =prompt("Enter First Integer" );
-	 a =parseFloat(a);
+	// let a =prompt("Enter First Integer" );
+	//  // a =parseFloat(a);
 	
-	let b=prompt("Enter Second Integer" );
-	 b =parseFloat(b);
+	// let b=prompt("Enter Second Integer" );
+	//  // b =parseFloat(b);
 	
-	const c = prompt("Multiplication is:",a*b) ;
-	const d = prompt("Division is:",a/b);
+	// const c = prompt("Multiplication is:",a*b) ;
+	// const d = prompt("Division is:",a/b);
 
 	
+ 	function sumandtriple(num1,num2){
+ 		let sum = num1+num2;
+ 		if(num1 == num2){
+ 			sum *=3;
+ 		}
+
+ 	 };
+
+ 	 console.log("Sum is ", sum);
+ 	 sumandtriple(4,5);
