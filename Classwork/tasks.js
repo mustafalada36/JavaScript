f6b025be5bed2054bf0afa5273abfe65005a9779
Muhammad Mustafa <mustafalada36@gmail.com>
2023-04-28 18:32:47 +0500

    update

--- Classwork/tasks.js
+++ Classwork/tasks.js
@@ -5,24 +5,7 @@
 	let b=prompt("Enter Second Integer" );
 	 b =parseFloat(b);
 	
-	const c = a*b;
-	const d = a/b;
-	console.log("Multiplication is:", +c) ;
-	console.log("Division is:", +d);
-	
-	// let num1 = prompt("Enter the first number:");
-	// let num2 = prompt("Enter the second number:");
-
-	// // convert the user input to numbers
-	// num1 = parseFloat(num1);
-	// num2 = parseFloat(num2);
-
-	// // calculate the multiplication and division
-	// const product = num1 * num2;
-	// const quotient = num1 / num2;
-
-	// // display the results
-	// console.log(`The product of ${num1} and ${num2} is ${product}`);
-	// console.log(`The quotient of ${num1} and ${num2} is ${quotient}`);
+	const c = prompt("Multiplication is:",a*b) ;
+	const d = prompt("Division is:",a/b);
 
 	
