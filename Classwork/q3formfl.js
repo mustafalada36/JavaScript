60a408803d8c37f1483918b113502e62b3ab7afc
Muhammad Mustafa <mustafalada36@gmail.com>
2023-04-29 20:40:08 +0500

    update

---
+++ Classwork/q3formfl.js
@@ -0,0 +1,7 @@
+//Q4
+ 	myfunction = function(){
+ 		var first, second;
+	    first = document.getElementById("firstname").value;
+	    second = document.getElementById("lastname").value;
+ 		document.getElementById("here").innerHTML = first+" "+second;
+ 	}
