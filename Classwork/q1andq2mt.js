d005faaa8da635d77a58d4d6495c9ad1e14e9a0a
Muhammad Mustafa <mustafalada36@gmail.com>
2023-04-29 20:40:02 +0500

    update

---
+++ Classwork/q1andq2mt.js
@@ -0,0 +1,37 @@
+	// Q1
+	let a =prompt("Enter First Integer" );
+	 // a =parseFloat(a);	
+	let b=prompt("Enter Second Integer" );
+	 // b =parseFloat(b);
+	const c = prompt("Multiplication is:",a*b) ;
+	const d = prompt("Division is:",a/b);
+
+
+
+
+	// Q2
+ 	function sumandtriple(num1,num2){
+ 		var sum = num1+num2;
+ 		if(num1 === num2){
+ 			sum *=3;
+ 		}
+ 		document.write("<br>");
+ 		document.write("<br>");
+ 		document.write("Sum = " + sum);
+
+ 	 };
+ 	 sumandtriple(4,4)();
+ 	
+
+
+
+	
+  
+ 	 
+
+ 	
+
+
+
+
+ 	
