0ec93ae2021098de8624bb2caf3981e05dbc0bd8
Muhammad Mustafa <mustafalada36@gmail.com>
2023-07-20 00:18:54 +0500

    Create variable.js

---
+++ NEW BY Professional/variable.js
@@ -0,0 +1,13 @@
+var petDog = "Rex";
+var petCat = "Pepper";
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is: ",petDog);
+console.log("My pet cat's name is: ",petCat);
+var catSound = "purr";
+var dogSound = "woof";
+console.log(petDog,"dogSound");
+console.log(petCat,"catSound");
+var catSound = "meow";
+console.log(petCat,"catSound");
+
