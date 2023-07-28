062c73909404008600c91dd4c44d5a0c306674c6
Muhammad Mustafa <mustafalada36@gmail.com>
2023-07-29 01:00:45 +0500

    update

---
+++ NEW BY Professional/Browser Concepts/Dom.js
@@ -0,0 +1,9 @@
+const h2 = document.createElement('h2');
+h2.innerText = "This is H2 Heading";
+h2.setAttribute('id','id1');
+h2.setAttribute('class','class1');
+document.body.appendChild(h2);
+
+
+
+document.querySelector('p','a');
